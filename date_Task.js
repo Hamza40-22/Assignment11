4f15f172b684f07050556513ec542c2feee29a60
Hamza40-22 <95353930+Hamza40-22@users.noreply.github.com>
2023-10-07 02:53:19 +0500

    Add files via upload

---
+++ date_Task.js
@@ -0,0 +1,76 @@
+ 
+
+
+// Question # 1
+
+
+// let mydate = new Date()
+// document.write(mydate)
+
+// ***************************************************     *****************************************
+
+
+// Question # 2
+
+      
+// let mydate = new Date()
+// let day = mydate.getDay()
+// document.write(getMonthName(day))
+// function  getMonthName(monthnumber){
+//       let monthName = ["Jauary","Februry"," March","April","May","Jun","July","August","September","October","November","December"]
+//       return monthName[monthnumber]
+      
+// }
+
+ // ***************************************************     *****************************************
+
+
+// Question # 3
+
+      
+// let mydate = new Date()
+// let day = mydate.getDay()
+// document.write(getDayName(day))
+// function  getDayName(daynumber){
+//       let dayName = ["Sunday","Monday"," Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+//       return dayName[daynumber]
+// }
+
+// ***************************************************     *****************************************
+
+
+// Question # 4
+
+
+// let date = new Date();
+// let week = date.getDay();
+
+// if(week ===6 ||week ===0){
+//       alert("Its Fun Day")
+// }else
+// alert("Its Not Fun Day")
+
+
+// ***************************************************     *****************************************
+
+
+// Question # 5
+
+// let date = new Date();
+// let month = date.getDay();
+
+// if(month<=15){
+//       document.write("First fifteen days of the month")
+// }else
+// alert("“Last days of the month”.")
+
+
+// ***************************************************     *****************************************
+
+
+// Question # 6
+ 
+// var currentDate = new Date();
+// var minutesSinceEpoch = (currentDate.getTime() - currentDate.getTimezoneOffset() * 60000) / 60000; 
+// document.write("Minutes since midnight, Jan. 1, 1970:", minutesSinceEpochmydate
---
+++ math_task.js
@@ -0,0 +1,76 @@
+
+// Question # 1
+
+// let user = +prompt("Enter Positive Number")
+
+//   if( user >=0){
+//     document.write("Number: "+user +"<br>"+"Round off value of the number: "+Math.round(user)+"<br>"+ "Floor value of the number: "+Math.floor(user)+"<br>"+"Ceil value of the number: "+Math.ceil(user))
+//   }
+
+
+
+
+// ***************************************************     *****************************************
+
+
+// Question # 2
+
+
+
+// let user = +prompt("Enter Negative Number")
+
+//   if( user <=-1){
+//     document.write("Number: "+user +"<br>"+"Round off value of the number: "+Math.round(user)+"<br>"+ "Floor value of the number: "+Math.floor(user)+"<br>"+"Ceil value of the number: "+Math.ceil(user))
+//   }
+
+
+
+
+// ***************************************************     *****************************************
+
+
+// Question # 3
+
+
+// let user = +prompt("Enter Number")
+//     document.write("Absolute value of : "+user+" is "+ Math.abs(user))
+
+
+
+
+ // ***************************************************     *****************************************
+
+
+// Question # 4
+
+// let round = Math.round(Math.random()* 6);
+// let round1 = Math.round(Math.random()* 6);
+//     document.write("Random dice value : "+round+"<br>"+ "Random dice value : "+round1)
+
+
+
+
+ // ***************************************************     *****************************************
+
+
+// Question # 5
+
+
+// let round = Math.round(Math.random()*2);
+
+// if(round <2 && Math.random()>1 ){
+//       document.write(round+"<br>"+"Random coin value : Heads ")
+// }
+// else
+// document.write(round+"<br>"+"Random coin value : Tails ")
+
+
+ // ***************************************************     *****************************************
+
+
+// Question # 6
+
+
+      // let round = Math.round(Math.random()*100);
+      //  document.write("Random number between 1 and 100: "+round)
+
